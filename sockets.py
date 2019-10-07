208b5cddf2f3268e08f15568471d81793fb938f2
MegGoodall <meg.goodall@gmail.com>
2019-10-07 20:32:58 +0200

    27210448 - Python socket created

---
+++ sockets.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+
+#create socket using the following
+
+def socket_create();
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
+        
